99cc8b93aea82cf611c0a60d9f336f9d125df955
trantuankc1 <trantuankc1@gmail.com>
2024-04-04 01:17:36 +0700

    bank detail admin

--- resources/views/vendor/s-cart-admin/sidebar.blade.php
+++ resources/views/vendor/s-cart-admin/sidebar.blade.php
@@ -2,7 +2,7 @@
    <aside class="main-sidebar sidebar-light-pink elevation-4 sidebar-no-expand">
     <!-- Brand Logo -->
     <a href="{{ sc_route_admin('admin.home') }}" class="brand-link navbar-secondary">
-      Simdata5g.com
+      {!! sc_config_admin('ADMIN_LOGO') !!}
     </a>
 
     <!-- Sidebar -->
