c40cdffb2f4f2b0bf844c81d0b6c04690b0f40aa
trantuankc1 <trantuankc1@gmail.com>
2024-04-20 00:32:42 +0700

    edit add core generator qr code

--- resources/views/templates/s-cart-light/screen/shop_order_success.blade.php
+++ resources/views/templates/s-cart-light/screen/shop_order_success.blade.php
@@ -19,6 +19,11 @@ $layout_page = shop_order_success
             <h2>{{ sc_language_render('checkout.order_success_msg') }}</h2>
             <h3>{{ sc_language_render('checkout.order_success_order_info', ['order_id'=>session('orderID')]) }}</h3>
         </div>
+        <div class="mt-2">
+            <h3>Qr Code</h3>
+            <img src="{{ asset('storage/' . $qr->image_qr_code) }}" alt="">
+        </div>
+       
     </div>
 </div>
 @endsection
