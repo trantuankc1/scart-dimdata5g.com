781c7b96da3e39c5b31c36bc2caad30e0b8a1a8f
trantuankc1 <trantuankc1@gmail.com>
2024-04-30 22:41:12 +0700

    update get qr code

--- resources/views/templates/s-cart-light/screen/shop_order_success.blade.php
+++ resources/views/templates/s-cart-light/screen/shop_order_success.blade.php
@@ -1,38 +1,39 @@
 @php
-/*
-$layout_page = shop_order_success
-**Variables:**
-- $orderInfo
-*/
+    /*
+    $layout_page = shop_order_success
+    **Variables:**
+    - $orderInfo
+    */
 @endphp
 
 @extends($sc_templatePath.'.layout')
 
 @section('block_main_content_center')
-<h6 class="aside-title">{{ $title }}</h6>
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <h2 class="title-page">{{ $title }}</h2>
+    <h6 class="aside-title">{{ $title }}</h6>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2 class="title-page">{{ $title }}</h2>
+            </div>
+            <div class="col-md-12 text-success">
+                <h2>{{ sc_language_render('checkout.order_success_msg') }}</h2>
+                <h3>{{ sc_language_render('checkout.order_success_order_info', ['order_id'=>session('orderID')]) }}</h3>
+            </div>
+            <div class="mt-2">
+                <h3>Qr Code</h3>
+                @foreach($qr as $qrCode)
+                    <img src="{{ asset('storage/' . $qrCode) }}" width="250px" alt="qr code" style="margin: 0 auto;">
+                @endforeach
+            </div>
         </div>
-        <div class="col-md-12 text-success">
-            <h2>{{ sc_language_render('checkout.order_success_msg') }}</h2>
-            <h3>{{ sc_language_render('checkout.order_success_order_info', ['order_id'=>session('orderID')]) }}</h3>
-        </div>
-        <div class="mt-2">
-            <h3>Qr Code</h3>
-            <img src="{{ asset('storage/' . $qr->image_qr_code) }}" alt="">
-        </div>
-       
     </div>
-</div>
 @endsection
 
 
 @push('styles')
-{{-- Your css style --}}
+    {{-- Your css style --}}
 @endpush
 
 @push('scripts')
-{{-- //script here --}}
+    {{-- //script here --}}
 @endpush
